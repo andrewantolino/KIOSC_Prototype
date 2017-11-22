727b1f3c658ca4971bed14f0598b843b39d7c7f3
Andrew Antolino <andrew.a@collectiveus.com>
2017-11-23 10:27:23 +1100

    Created DAL (Data Access Layer) folder, added ApplicationDbContext class.

---
+++ Kiosc_Prototype/DAL/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using Kiosc_Prototype.Models;
+
+namespace Kiosc_Prototype.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext() : base("Visualizer Context")
+        {
+            
+        }
+
+        public DbSet<Building> Buildings { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Gas> Gass { get; set; }
+        public DbSet<Water> Waters { get; set; }
+        public DbSet<ElectricityConsumed> ElectricityConsumeds { get; set; }
+        public DbSet<ElectricityProduced> ElectricityProdcueds { get; set; }
+        public DbSet<Temperature> Temperatures { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Kiosc_Prototype/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Kiosc_Prototype.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Kiosc_Prototype.DAL.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Kiosc_Prototype.DAL.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
